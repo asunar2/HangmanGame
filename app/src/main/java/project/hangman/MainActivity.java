5e076a90ceda0ca7870d97ca23680b7a55d0eddb
Matthew J Chou <matthew.chouj@gmail.com>
2018-11-27 16:18:07 -0600

    code for button

--- app/src/main/java/project/hangman/MainActivity.java
+++ app/src/main/java/project/hangman/MainActivity.java
@@ -2,12 +2,27 @@ package project.hangman;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 //this is a test
+    Button beginnerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        beginnerButton = findViewById(R.id.beginner); //change button to name of the beginner button thing
+
+        beginnerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //go to a new android page thing
+
+            }
+        });
     }
 }
