1d16c788d37a1c0cd50dd243128539b069f18669
AareanaReza <areza5@illinois.edu>
2018-11-28 18:56:26 -0600

    Added variables we need to keep track of during a game to GameActivity

--- app/src/main/java/project/hangman/GameActivity.java
+++ app/src/main/java/project/hangman/GameActivity.java
@@ -4,6 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class GameActivity extends AppCompatActivity {
+    //number of characters in the word
+    private int numCharacters;
+    //number of correct guesses
+    private int numCorrect;
+    //number of body parts
+    private int numBodyParts = 6;
+    //current body part (0 = head, 2 = body... 5 = one leg)
+    private int currentBodyPart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
