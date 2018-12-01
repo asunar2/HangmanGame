09bc74c422b99cb2b0d2515aa73029bc9e035449
Matthew Chou <matthew.chouj@gmail.com>
2018-12-01 17:29:45 -0600

    Made a WordMaker class, should get words from the internet... currently connects to OxfordDictionaries, but don't know how to get the word of the day from the site

--- app/src/main/java/project/hangman/GameActivity.java
+++ app/src/main/java/project/hangman/GameActivity.java
@@ -3,14 +3,22 @@ package project.hangman;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Arrays;
 
+import javax.net.ssl.HttpsURLConnection;
+
 public class GameActivity extends AppCompatActivity {
+    private static final String TAG = "GameActivity";
     //number of characters in the word
     private int numCharacters;
     //number of correct guesses
@@ -39,15 +47,51 @@ public class GameActivity extends AppCompatActivity {
         });
         //Remove this line later, update text on button press in future
         this.displayWrongLetters("Wrong Letters");
+        getWordOfDay();
     }
     private void displayWrongLetters(final String letters) {
+        //Change to uppercase
         String toDisplay = letters.toUpperCase();
+        //Change to an char[], sort by alphabetical
         char[] toSort = toDisplay.toCharArray();
         Arrays.sort(toSort);
+        //Convert back to a String and add spaces between letters to display
         toDisplay = new String(toSort);
         toDisplay = toDisplay.replace("", " ");
+        //Update the text in the TextView box
         TextView wrongLetters;
         wrongLetters = findViewById(R.id.wrongLetters);
         wrongLetters.setText(toDisplay);
     }
+    private static void getWordOfDay() {
+        String wordOfDay = null;
+        try {
+            //Open http connection
+            final URL url = new URL("https://en.oxforddictionaries.com/");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            //If the connection was successful, get the page and put it in a string for now
+            if (connection.getResponseCode() == 200) {
+                InputStream response = connection.getInputStream();
+                int charByte;
+                StringBuilder stringBuilder = new StringBuilder();
+                //Go until the end of the InputStream, then close the connection
+                while ((charByte = response.read()) != -1) {
+                    stringBuilder.append((char) charByte);
+                }
+                String output = stringBuilder.toString();
+                connection.disconnect();
+                Log.d(TAG, "OUTPUTTING: " + output);
+            } else {
+                Log.d(TAG, "http request failed, response code: " + connection.getResponseCode());
+            }
+
+
+        } catch(Exception e) {
+            Log.d(TAG, "oops: " + e);
+        }
+        //return wordOfDay;
+    }
+
 }
--- app/src/main/java/project/hangman/MainActivity.java
+++ app/src/main/java/project/hangman/MainActivity.java
@@ -1,15 +1,20 @@
 package project.hangman;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
-//this is a test
     Button beginnerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +31,7 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivity(myIntent);
             }
         });
+        //Needs to be run as an AsyncTask bc it requires network access
+        new WordMaker().execute();
     }
 }
